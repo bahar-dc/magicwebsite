a7dcc56c09dc9bf93d43702eb011b7fea2cf94aa
bahar <88434002+bahar-dc@users.noreply.github.com>
2026-02-20 15:00:53 +0500

    Worked  on the pricing page

--- public/assets/js/dark.js
+++ public/assets/js/dark.js
@@ -1,35 +1,44 @@
-(() => {
-  // src/assets/js/dark.js
-  function enableDarkMode() {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("theme", "dark");
-  }
-  function disableDarkMode() {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("theme", "light");
-  }
-  function isMobile() {
-    return window.matchMedia("(max-width: 2000px)").matches;
-  }
-  function detectColorScheme() {
-    if (isMobile()) {
-      enableDarkMode();
-      return;
-    }
-    let theme = "light";
-    if (localStorage.getItem("theme")) {
-      theme = localStorage.getItem("theme");
-    } else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      theme = "dark";
-    }
-    theme === "dark" ? enableDarkMode() : disableDarkMode();
-  }
-  detectColorScheme();
-  var toggleBtn = document.getElementById("dark-mode-toggle");
-  if (toggleBtn) {
-    toggleBtn.addEventListener("click", () => {
-      localStorage.getItem("theme") === "light" ? enableDarkMode() : disableDarkMode();
-    });
-  }
-})();
-//# sourceMappingURL=dark.js.map
+function enableDarkMode() {
+	document.body.classList.add('dark-mode');
+	localStorage.setItem('theme', 'dark');
+}
+
+function disableDarkMode() {
+	document.body.classList.remove('dark-mode');
+	localStorage.setItem('theme', 'light');
+}
+
+function isMobile() {
+	return window.matchMedia("(max-width: 2000px)").matches;
+}
+
+function detectColorScheme() {
+
+	if (isMobile()) {
+		enableDarkMode();
+		return;
+	}
+
+	let theme = 'light';
+
+	if (localStorage.getItem('theme')) {
+		theme = localStorage.getItem('theme');
+	}
+	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		theme = 'dark';
+	}
+
+	theme === 'dark' ? enableDarkMode() : disableDarkMode();
+}
+
+detectColorScheme();
+
+const toggleBtn = document.getElementById('dark-mode-toggle');
+
+if (toggleBtn) {
+	toggleBtn.addEventListener('click', () => {
+		localStorage.getItem('theme') === 'light'
+			? enableDarkMode()
+			: disableDarkMode();
+	});
+}
